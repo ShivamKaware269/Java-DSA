ee38e9d655536fcf7df917a94e4969de785d1c76
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-25 22:27:59 +0530

    Update Palindrome.java

--- 03-Functions And Methods/Palindrome.java
+++ 03-Functions And Methods/Palindrome.java
@@ -1,1 +1,35 @@
+import java.util.Scanner;
 
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int input = sc.nextInt();
+
+        int x = checkPalindrome(input);
+        if (x == 1) {
+            System.out.println(input + " is a palindrome number.");
+        } else {
+            System.out.println(input + " is not a palindrome number.");
+        }
+
+        sc.close();
+    }
+
+    static int checkPalindrome(int n) {
+        int originalNumber = n;
+        int reversedNumber = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            n /= 10;
+        }
+
+        if (originalNumber == reversedNumber) {
+            return 1; // Palindrome
+        } else {
+            return 0; // Not a palindrome
+        }
+    }   
+}
