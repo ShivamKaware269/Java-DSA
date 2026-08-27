0f2b6804b959cbd9c800f68d1c56965e2ad482a5
Shivam Kaware <shivamkaware269@gmail.com>
2026-08-27 22:10:38 +0530

    Update Armstrong_Number.java

--- 02-Conditionals And Loops/Intermediate Java Programs/Armstrong_Number.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Armstrong_Number.java
@@ -1,1 +1,27 @@
+import java.util.Scanner;
 
+public class Armstrong_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        int temp = num;
+        int sum = 0, rem = 0;
+
+        while (temp != 0) {
+            rem = temp % 10;
+            sum += Math.pow(rem, 3);
+            temp /= 10;
+        }
+
+        System.out.println(sum);
+
+        if (sum == num) {
+            System.out.println("The number is a Armstrong Number");
+        } else {
+            System.out.println("The number is not a Armstrong Number");
+        }
+        
+        sc.close();
+    }
+}
