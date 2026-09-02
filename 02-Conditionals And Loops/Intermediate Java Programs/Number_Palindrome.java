7569969b5248a5cff78247f345e9f0f4f62e7ee5
Shivam Kaware <shivamkaware269@gmail.com>
2026-09-02 21:35:36 +0530

    Implement number palindrome checker
    Add a program to check if a number is a palindrome.

--- 02-Conditionals And Loops/Intermediate Java Programs/Number_Palindrome.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Number_Palindrome.java
@@ -1,1 +1,26 @@
+import java.util.Scanner;
 
+public class Number_Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number :");
+        int num = sc.nextInt();
+        int rev = 0;
+        int temp = num;
+
+        while (temp != 0) {
+            int rem = temp % 10;
+            rev = rev*10 + rem;
+            temp = temp / 10;
+        }
+        System.out.println("The reverse of number is : " + rev);
+
+        if (num == rev) {
+            System.out.println("Number is Palindrome");
+        } else {
+            System.out.println("Number is not a Palindrome");
+        }
+
+        sc.close();
+    }
+}
