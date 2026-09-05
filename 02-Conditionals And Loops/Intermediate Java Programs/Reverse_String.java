b2551766c8c738f2f9c6a2a4eefbe67da3aa8141
Shivam Kaware <shivamkaware269@gmail.com>
2026-09-05 21:09:29 +0530

    Update Reverse_String.java

--- 02-Conditionals And Loops/Intermediate Java Programs/Reverse_String.java
+++ 02-Conditionals And Loops/Intermediate Java Programs/Reverse_String.java
@@ -1,1 +1,23 @@
+// Reverse a String in Java
 
+import java.util.Scanner;
+
+public class Reverse_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string to reverse: ");
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reverse);
+
+        sc.close();
+    }
+    
+}
